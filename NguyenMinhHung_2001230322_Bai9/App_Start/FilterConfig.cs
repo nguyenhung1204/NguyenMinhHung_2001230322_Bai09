60b7c0be997adba84cd14e19d082935a70d60a8c
nguyenhung1204 <nguyenminhhungh2o@gmail.com>
2025-11-06 18:32:22 +0700

    Commit lần 1

---
+++ NguyenMinhHung_2001230322_Bai9/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenMinhHung_2001230322_Bai9
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NguyenMinhHung_2001230322_Bai9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenMinhHung_2001230322_Bai9.MvcApplication" Language="C#" %>
